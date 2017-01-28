7b8a6550b441d07ecef4323fd6ab9f2a0d397618
betterclever <paliwal.pranjal83@gmail.com>
2017-01-28 23:29:34 +0530

    Made Ball Move

--- core/src/com/betterclever/libgdx/demo/MyGameScreen.java
+++ core/src/com/betterclever/libgdx/demo/MyGameScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -17,12 +18,17 @@ public class MyGameScreen implements Screen {
 	ExtendViewport viewport;
 	ShapeRenderer shapeRenderer;
 	
-	float radius = 10;
+	float radius = 20;
+	Vector2 position;
+	Vector2 velocity;
 	
 	@Override
 	public void show() {
 		viewport = new ExtendViewport(Constants.WORLD_WIDTH,Constants.WORLD_HEIGHT);
 		shapeRenderer = new ShapeRenderer();
+		
+		position = new Vector2(Constants.WORLD_WIDTH/2,Constants.WORLD_WIDTH/2);
+		velocity = new Vector2(1000,1000);
 	}
 	
 	@Override
@@ -32,14 +38,32 @@ public class MyGameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		
-		radius += 10*delta;
+		//radius += 10*delta;
 		
 		viewport.apply();
 		shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
 		
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.circle(Constants.WORLD_WIDTH/2,Constants.WORLD_HEIGHT/2,radius,256);
+		shapeRenderer.circle(position.x,position.y,radius,256);
 		shapeRenderer.end();
+		
+		position.mulAdd(velocity,delta);
+		
+		if(position.x + radius > Constants.WORLD_WIDTH){
+			velocity.x = -velocity.x;
+		}
+		
+		if(position.x - radius < 0){
+			velocity.x = -velocity.x;
+		}
+		
+		if(position.y + radius > Constants.WORLD_HEIGHT){
+			velocity.y = -velocity.y;
+		}
+		
+		if(position.y - radius < 0){
+			velocity.y = -velocity.y;
+		}
 	}
 	
 	@Override
